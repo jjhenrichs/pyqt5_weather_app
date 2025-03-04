8d9bb188e1568dae1a5c7444627a880debda4060
Jeff Henrichs <jeffhenrichs37@gmail.com>
2025-03-04 14:17:48 -0500

    fist commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys, requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Weather App")
+        self.setGeometry(700, 300, 500, 500)
+        self.initUI()
+
+    def initUI(self):
+        pass
+
+# This program will run only if main.py is called
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
